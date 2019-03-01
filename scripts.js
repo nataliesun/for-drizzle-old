531d1463241da246a95c0ef6ee1268cb94d3741a
natsun121 <natsun121@gmail.com>
2019-02-28 19:46:28 -0500

    setup form and fxn stubs

--- scripts.js
+++ scripts.js
@@ -1,1 +1,64 @@
-console.log("Hey");
+function cleanup() {
+  //owner: Julie
+  //cleans up polygons and resets form
+}
+
+function getRainForecast() {
+  //owner: Julie
+  //use zip code/coordinates
+  //api url: https://openweathermap.org/api
+}
+
+function getMoisture() {
+  //owner: Mengqi
+  //gets the moisture from polygon
+  //api url: https://agromonitoring.com/api
+}
+
+
+function getPolygon() {
+  //owner: Mengqi
+  //makes polygon from coordinates
+}
+
+
+function getCoordinates() {
+  //owner: Natalie
+  //make fetch call to getcoordinates passing in formatAddress
+  //api url: http://geoservices.tamu.edu
+}
+
+
+function formatAddress() {
+  //owner: Natalie
+  //gets address and turns into string that getCoordinates fxn needs it to be in
+  //street+city+state+zip
+}
+
+
+function displayResults() {
+  //fetch with chained callbacks
+
+  //1. call to formatAddress
+  //2. make call to getCoordinates
+  //3. getPolygon with coordinates
+  //4. getMoisture with polygon
+  //5. getRainForecast from address
+  //6. cleanup to reset form and clean up polygons
+}
+
+
+function watchForm() {
+
+
+}
+
+
+function main() {
+  console.log('App running');
+  watchForm();
+
+}
+
+
+$(main)
